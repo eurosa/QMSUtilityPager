790afbb9b4bb006e1cb21a2ab3c838419f6600fa
ranojan <radhasonk.phy@gmail.com>
2021-04-15 09:57:28 -0700

    Converted

---
+++ src/main/java/com/qms/utility/CounterLabel.java
@@ -0,0 +1,19 @@
+package com.qms.utility;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class CounterLabel extends Fragment {
+    public CounterLabel() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_counter_label, container, false);
+    }
+}
